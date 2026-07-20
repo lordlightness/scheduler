d7cf03c57e6b65fdd06fa0d1baf0e4cc9a3e8378
Dell (via Claude) <dev@rivermouth.local>
2026-07-20 13:29:49 +0000

    feat: add create/update/delete operations to EmployeeRepository

--- lib/data/repositories/employee_repository.dart
+++ lib/data/repositories/employee_repository.dart
@@ -8,4 +8,10 @@ class EmployeeRepository {
   Box<Employee> get _box => Hive.box<Employee>(HiveService.employeeBoxName);
 
   List<Employee> getAll() => _box.values.toList();
+
+  Future<void> add(Employee employee) => _box.put(employee.id, employee);
+
+  Future<void> update(Employee employee) => employee.save();
+
+  Future<void> delete(String id) => _box.delete(id);
 }
